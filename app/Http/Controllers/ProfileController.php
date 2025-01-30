b1030abab843543e1617443ef598cb35d3995b5e
Napwapp <abduhnawaf@gmail.com>
2025-01-30 10:44:53 +0700

    change-href

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
+    // untuk menampilkan halaman profil
     function profile() {
         return view('pointakses/user/profil');
     }
--- resources/views/pointakses/user/index.blade.php
+++ resources/views/pointakses/user/index.blade.php
@@ -42,7 +42,7 @@
                         <ul class="menu">
                             <li
                                 class="sidebar-item active">
-                                <a href="#" class='sidebar-link'>
+                                <a href="index.blade.php" class='sidebar-link'>
                                     <i class="bi bi-grid-fill"></i>
                                     <span>Dashboard<dist//span>
                                 </a>
